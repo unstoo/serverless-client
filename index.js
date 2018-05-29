c5d4a4b719c23722f4d8dfcb6e0ce624ccb452f0
Yevgeny <unstoo@gmail.com>
2018-05-29 21:01:16 +0300

    test get method

--- index.js
+++ index.js
@@ -113,9 +113,13 @@ function listFiles() {
   gapi.client.drive.files.list({
     'pageSize': 10,
     'fields': "nextPageToken, files(id, name)"
-  }).then(function(response) {
+  })
+  .then(function(response) {
+
     appendPre('Files:');
+
     var files = response.result.files;
+
     if (files && files.length > 0) {
       for (var i = 0; i < files.length; i++) {
         var file = files[i];
@@ -124,5 +128,16 @@ function listFiles() {
     } else {
       appendPre('No files found.');
     }
+
   });
+
+  gapi.client.drive.files.get({
+    'fileId': "12Qtb-8iuJzhJW-U48ZIaetODfRzRM771"
+  })
+  .then(function(response) {
+
+    console.log('GET RESPONSE')
+    console.log(response);
+    
+  })
 }
