b340556930e74b789309d58121ce96a74d67918b
Yevgeny <unstoo@gmail.com>
2018-05-29 20:11:46 +0300

    update

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function appendPre(message) {
 function listMajors() {
   gapi.client.sheets.spreadsheets.values.get({
     spreadsheetId: '1tsROLOxhB5U4nh6GDqgPPC76TwKCgV6Qxud4eWt1TUs',
-    range: 'Class Data!A1:E',
+    range: '2018!A1:B',
   }).then(function(response) {
     debugger
     var range = response.result;
